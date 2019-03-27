d9c03e4408e0493ea26a9df30232d44e687b767e
arul <arulisbehindyou@gmail.com>
2019-03-27 22:04:10 +0200

    fixed compton conf file location and changed blur strengths

--- scripts/scripts/apps.sh
+++ scripts/scripts/apps.sh
@@ -1,7 +1,7 @@
 redshift -l -26.009722:28.026111 &
 polybar burs &
 polybar bur &
-compton --config .compton.conf --blur-background &
+compton --config ~/.compton.conf --blur-background &
 autocutsel -fork &
 autocutsel -selection PRIMARY -fork &
 pactl set-sink-volume 0 50%
