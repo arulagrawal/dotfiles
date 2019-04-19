91c74fb2bbe89e013671bfcd3d182d053be46ab4
arul <arulisbehindyou@gmail.com>
2019-04-19 19:46:27 +0200

    ignore the json that contains my spotify information clean up spotify_add to use a spotify class

--- scripts/scripts/utility/spotify_add.py
+++ scripts/scripts/utility/spotify_add.py
@@ -1,52 +1,59 @@
 #!/usr/bin/python3
-import requests, base64, json
+import requests, base64, json, os, sys
 
 song_api_point = "https://api.spotify.com/v1/me/player"
 save_api_point = "https://api.spotify.com/v1/me/tracks"
 token_api_point = "https://accounts.spotify.com/api/token"
 
-r_token = "AQCHZ9aZBMqOyd2g-ArboVmOf1xQz-49pb5TyWDMMsLQK6VD7GZeiZkM55pUglmXx9BE7Mi7PfnwXaZO66r3I25L-aMBMgb9JFrx_qOc1lA4ciuYBa7-Lhy6M4n9NCIutVFZaA"
+
+class Spotify:
+
+    def loadDataFromFile(self):
+        path = os.path.join(sys.path[0] + '/spotifyinfo.json')
+        with open(path) as json_file:
+            info_json = json.load(json_file)
+            self.c_id = info_json['c_id']
+            self.c_secret = info_json['c_secret']
+            self.r_token = info_json['r_token']
+        
+        
+    def getClientString(self):
+        c_64 = self.c_id + ":" + self.c_secret
+        c_64 = base64.b64encode(c_64.encode())
+        c_64 = str(c_64)
+        c_64 = c_64[2:-1]
+        self.client_string = c_64
+
+    def getToken(self):
+        headers = {'Authorization': 'Basic ' + self.client_string}
+        payload = {'grant_type': 'refresh_token', 'refresh_token': self.r_token}
+        p = requests.post(token_api_point, headers=headers, data=payload)
+        token_json = json.loads(p.text)
+        self.my_token = str(token_json['access_token'])
+
+    def getCurrentSong(self):
+        params = {'market': 'ZA'}
+        headers = {'Authorization': 'Bearer ' + self.my_token}
+        self.song = requests.get(song_api_point, params=params, headers=headers).json()
 
 
-def getClientString():
-    c_id = "1b4882f18b5844c18f2b50790dde0bc1"
-    c_secret = "f3c13ff95c604199b2139cc165b5f73a"
-    c_64 = c_id + ":" + c_secret
-    c_64 = base64.b64encode(c_64.encode())
-    c_64 = str(c_64)
-    c_64 = c_64[2:-1]
-    return c_64
+    def addCurrentSongToLibrary(self):
+        self.getCurrentSong()
+        song = self.song['item']['uri']
+        song_id = song[14:]
+        params = {'ids': song_id}
+        headers = {'Authorization': 'Bearer ' + self.my_token}
+        requests.put(save_api_point, params=params, headers=headers)
 
-
-def getToken(client_string):
-    headers = {'Authorization': 'Basic ' + client_string}
-    payload = {'grant_type': 'refresh_token', 'refresh_token': r_token}
-    p = requests.post(token_api_point, headers=headers, data=payload)
-    token_json = json.loads(p.text)
-    my_token = str(token_json['access_token'])
-    return my_token
-
-
-def getCurrentSong(my_token):
-    params = {'market': 'ZA'}
-    headers = {'Authorization': 'Bearer ' + my_token}
-    r = requests.get(song_api_point, params=params, headers=headers)
-    return r.json()
-
-
-def addCurrentSongToLibrary(song, my_token):
-    song = song['item']['uri']
-    song_id = song[14:]
-    params = {'ids': song_id}
-    headers = {'Authorization': 'Bearer ' + my_token}
-    requests.put(save_api_point, params=params, headers=headers)
+    def __init__(self):
+        self.loadDataFromFile()
+        self.getClientString()
+        self.getToken()
 
 
 def main():
-    client = getClientString()
-    token = getToken(client)
-    song = getCurrentSong(token)
-    addCurrentSongToLibrary(song, token)
+    spotify = Spotify()
+    spotify.addCurrentSongToLibrary()
 
 
 if __name__ == "__main__":
